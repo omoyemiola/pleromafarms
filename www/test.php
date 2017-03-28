4af08c55ffed284356f2a37adec5db314e4982a3
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-03-28 23:58:25 +0100

    defining the file size for the image uploads

--- www/test.php
+++ www/test.php
@@ -18,11 +18,40 @@ try{
 
 	echo $e->getMessage();
 }*/
-
+	#max file size...
+	define("MAX_FILE_SIZE", "2097152");
+	
 	if(array_key_exists("save",$_POST)) {
 
-		print_r($_FILES);
+		$errors= [];
+
+	
+
+		#be sure a file was selected ...
+		if(empty($_FILES['pic']['name'])){
+
+			$errors[] = "please choose a file";
+		}
+
+		#check file size...
+
+		if($_FILES['pic']['size'] > MAX_FILE_SIZE){
+
+			$errors[] = "file size exceeds maximum.maximum: ".MAX_FILE_SIZE; 
+		}
+
+		if(empty($errors)){
+
+			echo "done";
+		} else {
+
+			foreach ($errors as $err) {
+				echo $err. '</br>';
+			}
+		}
+		//print_r($_FILES);
 	}
+
 	
 
 ?>
