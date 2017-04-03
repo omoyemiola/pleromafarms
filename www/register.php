f005f93c3a8c399967b91e93977c1a5dcf0955b3
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-04-03 12:39:08 +0100

    redefining the registration page

--- www/register.php
+++ www/register.php
@@ -12,9 +12,17 @@
 	</section>
 	
 	<div class="wrapper">
-		<h1 id="register-label">Admin Login</h1>
+		<h1 id="register-label">Admin Register</h1>
 		<hr>
-		<form id="register"  action ="login.php" method ="POST">
+		<form id="register"  action ="register.php" method ="POST">
+			<div>
+				<label>First name:</label>
+				<input type="text" name="fname" placeholder="last name">
+			</div>
+			<div>
+				<label>Last name:</label>
+				<input type="text" name="lname" placeholder="last name">
+			</div>
 			<div>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
@@ -23,17 +31,23 @@
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
+			<div>
+				<label>Confirm Password:</label>
+				<input type="password" name="password" placeholder="password">
+			</div>
 
-			<input type="submit" name="register" value="login">
+
+			<input type="submit" name="register" value="register">
 		</form>
 
-		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
-	</div>
+		</div>
+	<!--	<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
+	
 
 	<section class="foot">
 		<div>
 			<p>&copy; 2016</p>
 		</div>
-	</section>
+	</section> -->
 </body>
 </html>
