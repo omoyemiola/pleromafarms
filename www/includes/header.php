e4862ae939e472ac92fc00f723be35433395e3e9
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-04-03 11:53:45 +0100

    header.php file created

---
+++ www/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>
+	<section>
+		<div class="mast">
+			
+			<h1>T<span>SSB</span></h1>
+
+		</div>
+		</section>
+
+</body>
+</html>
